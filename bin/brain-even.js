45dfb9e29bfeeae445cde488e01f5a128e93ccba
Ivan <presimos@gmail.com>
2024-01-09 19:50:05 +0300

    some fix

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,14 +2,14 @@
 
 import { getName, getAnswer } from '../src/cli.js';
 import { getRandomNumber, compareAnswers } from '../src/index.js';
-import correctAnswer from '../src/games/even.js';
+import rightAnswer from '../src/games/even.js';
 
 const name = getName();
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 for (let i = 0; i <= 2; i += 1) {
-  const number = getRandomNumber(0, 100);
+  const number = getRandomNumber(1, 100);
   console.log(`Question: ${number}`);
-  if (compareAnswers(name, getAnswer(), correctAnswer(number))) {
+  if (compareAnswers(name, getAnswer(), rightAnswer(number))) {
     break;
   }
   if (i === 2) {
--- src/games/even.js
+++ src/games/even.js
@@ -1,3 +1,3 @@
-export default function correctAnswer(number) {
+export default function rightAnswer(number) {
   return number % 2 === 0 ? 'yes' : 'no';
 }
